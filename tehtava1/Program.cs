5b41968336ccad61ba4ab73db8acc5eea312eea5
k9279 <k9279@student.jamk.fi>
2018-01-22 16:37:12 +0200

    tankki toimii

---
+++ tehtava1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tank leopard = new tank();
+            tank kepard = new tank();
+            tank kamikard = new tank();
+            Console.WriteLine("LEOPARD 1337GRD");
+            leopard._crewcount = 4;
+            leopard.name = "kimmo";
+            leopard.type = "Erittain raskaasti panssaroitu kulkuneuvo";
+            Console.WriteLine("nopeus " + leopard.speed);
+            Console.WriteLine("crewcount: " + leopard.crewcount);
+            Console.WriteLine("nimi: " + leopard.name);
+            Console.WriteLine("tyyppi: " + leopard.type);
+            leopard.accerelateto(20);
+            Console.WriteLine("painetaampas leopardin kaasupoljin pohjasta lapi... ");
+            Console.WriteLine("nopeus " + leopard.speed);
+            Console.WriteLine("\nKEPARD 69-dd");
+            kepard._crewcount = 6;
+            kepard.name = "jorma";
+            kepard.type = "Erittain raskaasti panssaroitu kulkuneuvo joka kulkee taysia";
+            Console.WriteLine("nopeus " + kepard.speed);
+            Console.WriteLine("crewcount: " + kepard.crewcount);
+            Console.WriteLine("nimi: " + kepard.name);
+            Console.WriteLine("tyyppi: " + kepard.type);
+            kepard.accerelateto(70);
+            Console.WriteLine("runtataampas kepardin kaasu kohti kaakkoa... ");
+            Console.WriteLine("nopeus " + kepard.speed);
+            Console.WriteLine("\nKAMIKARD 420-blazzeyit");
+            kamikard._crewcount = 2;
+            kamikard.name = "zorgos";
+            kamikard.type = "Panssaroimaton kulkuneuvo joka kulkee sata kaks sataa";
+            Console.WriteLine("nopeus " + kamikard.speed);
+            Console.WriteLine("crewcount: " + kamikard.crewcount);
+            Console.WriteLine("nimi: " + kamikard.name);
+            Console.WriteLine("tyyppi: " + kamikard.type);
+            kamikard.accerelateto(200);
+            Console.WriteLine("Kokeillaan kamikardin kaasupoljinta hellasti... ");
+            Console.WriteLine("nopeus " + kamikard.speed);
+
+
+
+        }
+    }
+}
---
+++ tehtava1/tank.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava1
+{
+    class tank
+    {
+        int min = 2;
+        int max = 6;
+        public string name { get; set; }
+        public string type { get; set; }
+        public int _crewcount;
+        public int crewcount
+        {
+            get { return _crewcount; }
+            set
+            {
+                if (_crewcount < min)
+                {
+
+                    _crewcount = min;
+                }
+                if (_crewcount > max)
+                {
+                   _crewcount = max;
+                }
+            }
+
+
+        }
+
+        public float _speed;
+        public float speed
+        {
+          
+            get { return _speed; }
+            set
+            {
+                _speed = 0;
+            }
+        }
+        readonly float maxspeed = 100;
+        public void accerelateto(float n)
+        {
+            _speed = (int)n;
+           if(_speed > maxspeed)
+            {
+                _speed = 100;
+            }
+        }
+        public void slowto(float i)
+        {
+              _speed = (int) i;
+        }
+    }
+}
